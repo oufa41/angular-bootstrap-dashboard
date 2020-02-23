81ea186547cb0237a7ca3adb0644d807d3b0903b
ABD EL RAOUF Ahmed <Ahmed.ABDELRAOUF@cegedim.com>
2020-02-23 14:13:47 +0200

    handle side bar

--- src/app/side-vertical-navbar/side-vertical-navbar.component.ts
+++ src/app/side-vertical-navbar/side-vertical-navbar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-vertical-navbar.component.css']
 })
 export class SideVerticalNavbarComponent implements OnInit {
-
+  names = ['oufa', 'ali', 'ahmed'];
+  leafs = ['1', '2', '3'];
+  itemClicked: string;
   constructor() { }
 
   ngOnInit() {
   }
+  leafClick(id: string) {
 
+    if (this.itemClicked === id) {
+      this.itemClicked = null;
+    } else {
+      this.itemClicked = id;
+    }
+
+  }
 }
